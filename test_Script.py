256ae64a33ea352800fde8811ee67476e2ed6c63
issac-kondreddy <ixk93660@ucmo.edu>
2024-09-24 10:15:08 -0500

    Added libs

--- test_Script.py
+++ test_Script.py
@@ -1,10 +1,15 @@
 import unittest
+import xmlrunner
+import os
 
 class TestScript(unittest.TestCase):
     def test_output(self):
         self.assertEqual("Hello from GitHub!", "Hello from GitHub!")
 
 if __name__ == '__main__':
+    # Ensure the directory exists
     os.makedirs('test-reports', exist_ok=True)
+
+    # Run the tests and generate the XML report
     with open('test-reports/results.xml', 'wb') as output:
         unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output), failfast=False, buffer=False, catchbreak=False)
