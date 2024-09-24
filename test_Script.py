caad8fbd6bf8ce6a460ea702da9d389ea9b751dc
issac-kondreddy <ixk93660@ucmo.edu>
2024-09-24 10:04:49 -0500

    Added Requirement.txt and modified test script by adding xml format to display

--- test_Script.py
+++ test_Script.py
@@ -5,4 +5,5 @@ class TestScript(unittest.TestCase):
         self.assertEqual("Hello from GitHub!", "Hello from GitHub!")
 
 if __name__ == '__main__':
-    unittest.main()
+    with open('test-reports/results.xml', 'wb') as output:
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output), failfast=False, buffer=False, catchbreak=False)
